cb21552df077f2bdb27be10ab05ec6944c103539
KoelieOelie <you@example.com>
2017-07-13 19:41:59 +0200

    add lib

---
+++ display.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script src="https://aframe.io/releases/0.2.0/aframe.min.js"></script>
+    <script src="./js/core.js"></script>
+  </head>
+  <body>
+
+  </body>
+</html>
