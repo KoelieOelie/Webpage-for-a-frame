401c57d2a572516cbb95e15904300d774c4614d0
KoelieOelie <Kevoelie@gmail.com>
2017-07-14 17:59:48 +0200

    Add files via upload

--- display.php
+++ display.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <script src="https://aframe.io/releases/0.2.0/aframe.min.js"></script>
-    <script src="./js/core.js"></script>
-  </head>
-  <body>
+<head>
+<title>My A-Frame Scene</title>
+<script src="https://aframe.io/releases/0.5.0/aframe.min.js"></script>
+<script src="https://unpkg.com/aframe-html-shader@0.2.0/dist/aframe-html-shader.min.js"></script>
+</head>
 
-  </body>
+<body>
+<a-scene>
+  <a-entity geometry="primitive: box" material="shader: html; target: #htmlElement"></a-entity>
+</a-scene>
+
+<div style="width: 100%; height: 100%; position: fixed; left: 0; top: 0; z-index: -1; overflow: hidden">
+  <div id="htmlElement" style="background: #F8F8F8; color: #333; font-size: 48px">HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH</div>
+</div>
+
+</body>
 </html>
